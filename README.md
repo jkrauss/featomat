e94fdae87ba6a20602e9d1eb0ead1738c9e644c1
Jonas Krauss <mail@jonaskrauss.de>
2023-11-24 17:06:28 +0100

    project structure

---
+++ README.md
@@ -0,0 +1,6 @@
+
+- The PyPI name does not need to match the package name
+- The PyPI name must be unique
+- the package name will be imported
+
+- use setuptools as a build system
---
+++ src/__main__.py
@@ -0,0 +1,8 @@
+# If you’re creating a package that’s supposed to be executed, 
+# then you should include a __main__.py file
+
+def main():
+    print('Hooray!')
+
+if __name__ == '__main__':
+    main()
